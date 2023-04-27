11f03fa8fa00c902fedfec7968f22b07b52684d6
Anasa <hamidlmath534@gmail.com>
2023-04-27 18:30:50 +0100

    firstPush

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import RecipeDetailScreen from "./Screens/Restaurant/RecipeDetailScreen";
+import DATA from "./config/Restaurant/DATA";
+import HomeScreen from "./Screens/Restaurant/HomeScreen";
+import WelcomeScreen from "./Screens/Restaurant/WelcomeScreen";
+const App = () => {
+  // return <RecipeDetailScreen recipe={DATA[0].recipes[1]} />;
+  return <HomeScreen />
+};
+
+export default App;
