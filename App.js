3672615cfd0c10ece602f9138be0cf558445beca
Anasa <hamidlmath534@gmail.com>
2023-04-27 19:46:36 +0100

    test

--- App.js
+++ App.js
@@ -3,9 +3,21 @@ import RecipeDetailScreen from "./Screens/Restaurant/RecipeDetailScreen";
 import DATA from "./config/Restaurant/DATA";
 import HomeScreen from "./Screens/Restaurant/HomeScreen";
 import WelcomeScreen from "./Screens/Restaurant/WelcomeScreen";
-const App = () => {
-  // return <RecipeDetailScreen recipe={DATA[0].recipes[1]} />;
-  return <HomeScreen />
-};
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+export default App=()=>{
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Home'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
 
-export default App;
+
